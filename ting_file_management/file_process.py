81f420e5a23516b0c415b1ffaee04675b8f2eeb9
ottomicheletti <michelettiotto@gmail.com>
2022-12-01 17:35:25 -0300

    feat(Req 03): :sparkles: Implements process function within file_process.py
    The process function is responsible to verify if the wanted data to be processed already exists in the queue. If not, the data is enqueued and the data is writen to the stdout.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+# from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    imported_text = txt_importer(path_file)
+
+    data = {
+        "nome_do_arquivo": f"{path_file}",
+        "qtd_linhas": len(imported_text),
+        "linhas_do_arquivo": imported_text,
+    }
+
+    if data in instance.queue:
+        return None
+
+    instance.enqueue(data)
+    sys.stdout.write(str(data))
 
 
 def remove(instance):
