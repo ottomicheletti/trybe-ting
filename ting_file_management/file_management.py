cda67424b69f4964b879d66ad463ca28a1490348
ottomicheletti <michelettiotto@gmail.com>
2022-12-01 16:48:34 -0300

    feat(Req 02): :sparkles: Implements the function txt_importer within file_management.py
    The function firstly checks if the path_file call in the parameter is an existent file. If it doesn't exist, it writes in the stderr "Arquivo {path_file} não encontrado\n". After validating whether the files exists, the function tests for the file extension. If the file doesn't have a ".txt" extension, it returns "Formato inválido\n" on the stderr. After all the tests, if they pass, the function returns a list with the lines from the .txt file. Also, for an expected output, the lines were formatted with the .strip() method for taking off the \n character at the end of each line.

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+import os
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not os.path.isfile(path_file):
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+    _, file_extension = os.path.splitext(path_file)
+
+    if file_extension != ".txt":
+        return sys.stderr.write("Formato inválido\n")
+
+    with open(path_file) as txt_file:
+        lines = [line.strip() for line in txt_file.readlines()]
+        return lines
