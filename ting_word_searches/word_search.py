0ea95466bfb6e17852876f6a3cb75a42faba97ae
ottomicheletti <michelettiotto@gmail.com>
2022-12-02 02:55:48 -0300

    feat(Req 07): :sparkles: Implements search_by_word function within word_search.py
    I've repeated the sctructure from Req 06 and just added a new key inside the dict of "ocorrencias". Now, besides returning the index of the line where the word was found, it also returns the full line in a key called "conteudo".

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -21,4 +21,25 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+
+    for data in instance.queue:
+        result.append(
+            {
+                "arquivo": data["nome_do_arquivo"],
+                "ocorrencias": [
+                    {
+                        "linha": index + 1,
+                        "conteudo": line,
+                    }
+                    for index, line in enumerate(data["linhas_do_arquivo"])
+                    if word.lower() in line.lower()
+                ],
+                "palavra": word.lower(),
+            }
+        )
+
+    if not result[0]["ocorrencias"]:
+        return []
+
+    return result
