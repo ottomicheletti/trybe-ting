2d7e0cefb3c2c3f115e4e2a06278f09386eec93a
ottomicheletti <michelettiotto@gmail.com>
2022-12-02 02:51:43 -0300

    feat(Req 06): :sparkles: Implements exists_word function within word_search.py
    The existis_word function iterates over the queue list testing if the word passed as an argument exists within the lines existing inside the list "linhas_do_arquivo". If true, the list "ocorrencias" will be populated with dicts containing the index of the line where the word was found. If the "ocorrencias" iteration fails to encounter any match, the function just returns an empty list.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+
+    for data in instance.queue:
+        result.append(
+            {
+                "arquivo": data["nome_do_arquivo"],
+                "ocorrencias": [
+                    {"linha": index + 1}
+                    for index, line in enumerate(data["linhas_do_arquivo"])
+                    if word.lower() in line.lower()
+                ],
+                "palavra": word.lower(),
+            }
+        )
+
+    if not result[0]["ocorrencias"]:
+        return []
+
+    return result
 
 
 def search_by_word(word, instance):
